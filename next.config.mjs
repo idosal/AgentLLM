d68cb18f8622a7cbd0a5a047b441484f92d414b6
Ido Salomon <idosalomon@gmail.com>
2023-04-24 02:37:32 +0300

    build: skip env validation

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+// !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -14,7 +14,7 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
-  webpack: function (config, options) {
+  webpack: function (config, _) {
     config.experiments = { asyncWebAssembly: true, layers: true, topLevelAwait: true };
     return config;
   }
